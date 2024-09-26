2fd846f13b04744beae7b1e1386fc71796cf410b
oghene-kevwe <idisepraise@gmail.com>
2024-09-26 15:37:44 +0100

    axios bug fix
    Signed-off-by: oghene-kevwe <idisepraise@gmail.com>

--- Mercators Landing Page/src/components/ContactUs.jsx
+++ Mercators Landing Page/src/components/ContactUs.jsx
@@ -1,51 +1,81 @@
-import React, { useRef } from 'react'
-import { useGlobalContext } from '../context';
-import axios from 'axios';
+import React, { useRef } from "react";
+import { useGlobalContext } from "../context";
 
-const url = 'https://mercatorapiservices.com/api/Message/Send'
+const url = "https://mercatorapiservices.com/api/Message/Send";
 const ContactUs = () => {
-  const { email, name, message, changeEmail, changeName, changeMessage, sendEmail} = useGlobalContext()
-  
-//  const sendEmail =async(e)=>{
-//     e.preventDefault()
+  const {
+    email,
+    name,
+    message,
+    changeEmail,
+    changeName,
+    changeMessage,
+    sendEmail,
+  } = useGlobalContext();
 
-//  }
- 
   return (
-    <div className='hero '>
+    <div className="hero ">
       {/* contact-center  */}
-      <div className='text-center py-28 '  data-aos="fade-up"
-    data-aos-duration="2000">
+      <div
+        className="text-center py-28 "
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
         {/* heading  */}
-     <h1 className='uppercase text-2xl sms:text-3xl text-white font-700 contact '>contact us</h1>
-     <p className='text-white mt-4 p-5 max-w-[28rem] sms:max-w-[33rem] mx-auto'>We are available anyday, anytime, feel free to reach out to us. You
-can drop a message, we will respond as soon as possible.
-Fill the form to drop a message with us.</p>
+        <h1 className="uppercase text-2xl sms:text-3xl text-white font-700 contact ">
+          contact us
+        </h1>
+        <p className="text-white mt-4 p-5 max-w-[28rem] sms:max-w-[33rem] mx-auto">
+          We are available anyday, anytime, feel free to reach out to us. You
+          can drop a message, we will respond as soon as possible. Fill the form
+          to drop a message with us.
+        </p>
 
-    {/* form-center  */}
-    <form className='mt-5 ' onSubmit={sendEmail} >
-        {/* input  */}
-       <div>
-      <input type="text" name='user_email' value={email} onChange={changeEmail} placeholder='Email address' className='w-[80%] max-w-[600px] p-[.35rem] pl-5 rounded-md focus:outline-none' />
-       </div>
-        {/* input  */}
-       <div className='mt-2'>
-      <input type="text" name='from_name' value={name} onChange={changeName} placeholder='Full Name' className='w-[80%] max-w-[600px] p-[.35rem] pl-5 rounded-md focus:outline-none' />
-       </div>
-        {/* input  */}
-       <div className='mt-2'>
-      <input type="text" name='message' value={message} onChange={changeMessage} placeholder='Message' className='w-[80%] max-w-[600px] p-[.35rem] pl-5 rounded-md pb-40 focus:outline-none' />
-       </div>
-     
-      {/* submit button  */}
-       <button className='mt-6 bg-btnColor rounded-md px-6 py-2'>
-          <p className='text-white  '>Submit</p>
-       </button>
-    </form>
-    {/* end of form center  */}
+        {/* form-center  */}
+        <form className="mt-5 " onSubmit={sendEmail}>
+          {/* input  */}
+          <div>
+            <input
+              type="text"
+              name="user_email"
+              value={email}
+              onChange={changeEmail}
+              placeholder="Email address"
+              className="w-[80%] max-w-[600px] p-[.35rem] pl-5 rounded-md focus:outline-none"
+            />
+          </div>
+          {/* input  */}
+          <div className="mt-2">
+            <input
+              type="text"
+              name="from_name"
+              value={name}
+              onChange={changeName}
+              placeholder="Full Name"
+              className="w-[80%] max-w-[600px] p-[.35rem] pl-5 rounded-md focus:outline-none"
+            />
+          </div>
+          {/* input  */}
+          <div className="mt-2">
+            <input
+              type="text"
+              name="message"
+              value={message}
+              onChange={changeMessage}
+              placeholder="Message"
+              className="w-[80%] max-w-[600px] p-[.35rem] pl-5 rounded-md pb-40 focus:outline-none"
+            />
+          </div>
+
+          {/* submit button  */}
+          <button className="mt-6 bg-btnColor rounded-md px-6 py-2">
+            <p className="text-white  ">Submit</p>
+          </button>
+        </form>
+        {/* end of form center  */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
